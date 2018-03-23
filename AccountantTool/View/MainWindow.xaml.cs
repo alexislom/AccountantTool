4166d34898781583e999f1a2384c7d0227e61cce
alexislom <alexislomako@gmail.com>
2018-03-23 14:20:13 +0300

    Update UI in MainWindow.xaml

--- AccountantTool/View/MainWindow.xaml.cs
+++ AccountantTool/View/MainWindow.xaml.cs
@@ -1,15 +1,22 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using AccountantTool.ViewModel;
+using MahApps.Metro.Controls;
 
 namespace AccountantTool
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var currentLanguage = App.SelectedLanguage;
+            App.SelectedLanguage = new CultureInfo("en-US");
+            App.SelectedLanguage = new CultureInfo("ru-RU");
+            DataContext = new MainWindowViewModel();
         }
     }
 }
