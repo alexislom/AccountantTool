13e3e918c078a42b8c367e9012ddf4906e1fb7b0
aliaksei.lamaka <aliaksei.lamaka@gambinoslot.com>
2018-03-23 12:09:10 +0300

    Add template for ORM layer

---
+++ AccountantTool/Data/AccountantDBContext.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity;
+
+namespace AccountantTool.Data
+{
+    public class AccountantDbContext : DbContext
+    {
+        #region Construction
+
+        public AccountantDbContext() : base("AccountantDb")
+        {
+            Configure();
+        }
+
+        #endregion Construction
+
+        #region Configuration
+
+        private void Configure()
+        {
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.LazyLoadingEnabled = true;
+        }
+        #endregion Configuration
+
+        #region Initialization
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var initializer = new AccountantDbInitializer(modelBuilder);
+            Database.SetInitializer(initializer);
+        }
+        #endregion Initialization
+
+        #region Public Properties (tables)
+
+        //public DbSet<Manufacturer> Manufacturers { get; set; }
+        //public DbSet<Medicament> Medicaments { get; set; }
+        //public DbSet<MedicamentRecord> MedicamentRecords { get; set; }
+
+        #endregion Public Properties (tables)
+    }
+}
---
+++ AccountantTool/Data/AccountantDbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using SQLite.CodeFirst;
+
+namespace AccountantTool.Data
+{
+    public class AccountantDbInitializer : SqliteDropCreateDatabaseWhenModelChanges<AccountantDbContext>
+    {
+        #region Construction
+
+        public AccountantDbInitializer(DbModelBuilder modelBuilder) : base(modelBuilder)
+        {
+        }
+
+        #endregion Construction
+
+        #region Initialization
+
+        //TODO: Create stubs here
+        protected override void Seed(AccountantDbContext context)
+        {
+            //context.//DbSet.Add(lalala);
+            //context.SaveChanges();
+            base.Seed(context);
+        }
+
+        #endregion Initialization
+    }
+}
