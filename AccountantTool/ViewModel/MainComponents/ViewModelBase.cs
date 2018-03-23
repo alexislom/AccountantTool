32b2685175a66f79ebc5852806c9fec38d1009ea
aliaksei.lamaka <aliaksei.lamaka@gambinoslot.com>
2018-03-23 11:58:06 +0300

    Add project files.

---
+++ AccountantTool/ViewModel/MainComponents/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AccountantTool.ViewModel.MainComponents
+{
+    /// <summary>
+    /// Base ViewModel
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AccountantTool/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using AccountantTool.ViewModel.MainComponents;
+
+namespace AccountantTool.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        private ICommand _command;
+
+        public ICommand Command
+        {
+            get
+            {
+                return _command ?? (_command = new RelayCommand(
+                           x =>
+                           {
+                               DoStuff();
+                           }));
+            }
+
+        }
+
+        private static void DoStuff()
+        {
+            MessageBox.Show("Responding to button click event...");
+        }
+    }
+}
