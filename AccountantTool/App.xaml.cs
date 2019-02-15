577c13d4c05fa5a79919308248750c7ab6a7a845
alexislom <alexislomako@gmail.com>
2019-02-15 16:23:39 +0300

    Catch unhandled exceptions and write log to file

--- AccountantTool/App.xaml.cs
+++ AccountantTool/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Windows;
@@ -29,6 +30,29 @@ namespace AccountantTool
             Languages.Add(new CultureInfo("ru-RU"));
 
             SelectedLanguage = AccountantTool.Properties.Settings.Default.Language;
+
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            try
+            {
+                var dtLogFileCreated = DateTime.Now;
+                var sw = new StreamWriter("crash-" + dtLogFileCreated.Day + dtLogFileCreated.Month
+                                                   + dtLogFileCreated.Year + "-" + dtLogFileCreated.Second
+                                                   + dtLogFileCreated.Minute + dtLogFileCreated.Hour + ".txt");
+
+                var ex = (Exception)unhandledExceptionEventArgs.ExceptionObject;
+
+                sw.WriteLine("### Server Crash ###");
+                sw.WriteLine(ex.Message + ex.StackTrace);
+                sw.Close();
+            }
+            finally
+            {
+                Application.Current.Shutdown();
+            }
         }
 
         public static CultureInfo SelectedLanguage
