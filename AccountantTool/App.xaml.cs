2b693e5ad4a946efa3be520786c5e464f6883753
alexislom <alexislomako@gmail.com>
2019-02-15 16:36:31 +0300

    Clean up

--- AccountantTool/App.xaml.cs
+++ AccountantTool/App.xaml.cs
@@ -51,7 +51,7 @@ namespace AccountantTool
             }
             finally
             {
-                Application.Current.Shutdown();
+                Current.Shutdown();
             }
         }
 
